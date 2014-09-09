6baae144fc6333e68ed9fc5d4a93079d69c6cc87
Suman Tripathi <stripath@amcc.com>
2014-09-08 22:21:09 -0700

    MdeModulePkg: Implementing the link down retry workaround in tianocore.
    Signed-off-by: Tuan Phan <tphan@apm.com>

--- MdeModulePkg/Bus/Ata/AtaAtapiPassThru/AhciMode.c
+++ MdeModulePkg/Bus/Ata/AtaAtapiPassThru/AhciMode.c
@@ -2167,6 +2167,72 @@ Error6:
 
   return Status;
 }
+UINT32
+EFIAPI
+AhciSendComreset (
+ EFI_PCI_IO_PROTOCOL		*PciIo,
+ UINT8 				 Port 
+)
+{
+	UINT32 Offset;
+	UINT32 Val, timeout = 0;
+
+	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SCTL;
+	AhciOrReg(PciIo, Offset, 0x4);
+      	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_CMD;
+	AhciAndReg(PciIo, Offset, 0xfffffffe);
+	AhciOrReg(PciIo, Offset, 0x1);
+	AhciAndReg(PciIo, Offset, 0xfffffffe);
+
+	MicroSecondDelay((1000));
+	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SCTL;
+	AhciOrReg(PciIo, Offset, 0x1);
+
+	AhciAndReg(PciIo, Offset, 0xfffffff0);
+	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SCTL;
+	Val = AhciReadReg(PciIo, Offset);
+	AhciWriteReg(PciIo, Offset, Val);
+	MicroSecondDelay((1000));
+
+	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SERR;
+	Val = AhciReadReg(PciIo, Offset);
+
+	/* Wait for COMINIT and COMWAKE */
+	timeout = 1000;
+	do {
+		Val = AhciReadReg(PciIo, Offset);
+		if ((Val & EFI_AHCI_PORT_SERR_EX) && (Val & EFI_AHCI_PORT_SERR_CW))
+			break;
+
+		MicroSecondDelay((1000));
+	} while (--timeout > 0);
+
+
+	Val = AhciReadReg(PciIo, Offset);
+	if (!(Val & EFI_AHCI_PORT_SERR_CW))
+		DEBUG((EFI_D_ERROR, "COMWAKE[port%d] not detected\n", Port));
+
+	/* DEVICE PRESENCE */
+	timeout = 5;
+	do {
+      		Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SSTS;
+		Val = AhciReadReg(PciIo, Offset);
+		if ( (Val & EFI_AHCI_PORT_SCTL_DET_MASK) == 0x3) {
+			DEBUG((EFI_D_INFO, "Device Presence detected\n", Port));
+			break;
+		}
+
+		MicroSecondDelay((1000));
+	} while (--timeout > 0);
+
+	Val = AhciReadReg(PciIo, Offset);
+	if ((Val & EFI_AHCI_PORT_SCTL_DET_MASK) != 0x3) {
+		DEBUG((EFI_D_ERROR, "Speed negotiation is failuture xgene_port_send_comreset\n"));
+		return -1;
+	}
+
+	return 0;
+}
 
 /**
   Initialize ATA host controller at AHCI mode.
@@ -2200,6 +2266,7 @@ AhciModeInitialization (
   EFI_ATA_COLLECTIVE_MODE          *SupportedModes;
   EFI_ATA_TRANSFER_MODE            TransferMode;
   UINT32                           PhyDetectDelay;
+  UINT8 issue_comreset;
 
   if (Instance == NULL) {
     return EFI_INVALID_PARAMETER;
@@ -2256,7 +2323,8 @@ AhciModeInitialization (
       }
 
       IdeInit->NotifyPhase (IdeInit, EfiIdeBeforeChannelEnumeration, Port);
-
+       issue_comreset = 3;
+comreset_retry:
       //
       // Initialize FIS Base Address Register and Command List Base Address Register for use.
       //
@@ -2336,6 +2404,15 @@ AhciModeInitialization (
         // No device detected at this port.
         // Clear PxCMD.SUD for those ports at which there are no device present.
         //
+	Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_SERR;
+	Data = AhciReadReg (PciIo, Offset);
+	if (issue_comreset && (Data & EFI_AHCI_PORT_SERR_EX)) {
+		--issue_comreset;
+		DEBUG ((EFI_D_ERROR, "Device presence detected but speed negotiation failure issue comreset = %d \n", 
+			issue_comreset));
+		AhciSendComreset(PciIo, Port);
+		goto comreset_retry;
+	}	
         Offset = EFI_AHCI_PORT_START + Port * EFI_AHCI_PORT_REG_WIDTH + EFI_AHCI_PORT_CMD;
         AhciAndReg (PciIo, Offset, (UINT32) ~(EFI_AHCI_PORT_CMD_SUD));
         continue;
@@ -2380,7 +2457,7 @@ AhciModeInitialization (
       if (EFI_ERROR (Status)) {
         continue;
       }
-
+	
       Data = AhciReadReg (PciIo, Offset);
       if ((Data & EFI_AHCI_ATAPI_SIG_MASK) == EFI_AHCI_ATAPI_DEVICE_SIG) {
         Status = AhciIdentifyPacket (PciIo, AhciRegisters, Port, 0, &Buffer);

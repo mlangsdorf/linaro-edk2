98b61a10d822a7002994427e4b3c9613840c5856
Laszlo Ersek <lersek@redhat.com>
2014-12-27 21:01:43 +0100

    APMXGenePkg/AcpiPlatformDxe: wrap INF file and main C file at 79 chars
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>

--- ArmPlatformPkg/APMXGenePkg/AcpiPlatformDxe/AcpiPlatform.c
+++ ArmPlatformPkg/APMXGenePkg/AcpiPlatformDxe/AcpiPlatform.c
@@ -1,15 +1,15 @@
 /** @file
   APM X-Gene ACPI Platform Driver
 
-  Copyright (c) 2014, Applied Micro Curcuit Corporation. All rights reserved.<BR>
-  This program and the accompanying materials
-  are licensed and made available under the terms and conditions of the BSD License
-  which accompanies this distribution.  The full text of the license may be found at
+  Copyright (c) 2014, Applied Micro Curcuit Corporation. All rights reserved.
+
+  This program and the accompanying materials are licensed and made available
+  under the terms and conditions of the BSD License which accompanies this
+  distribution.  The full text of the license may be found at
   http://opensource.org/licenses/bsd-license.php
 
-  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+  WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 **/
 #include <PiDxe.h>
 #include <Guid/EventGroup.h>
